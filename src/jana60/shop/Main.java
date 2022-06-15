75467279f245336960618b815ed86e1daf056515
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-15 12:15:06 +0200

    esercizio completo

--- src/jana60/shop/Main.java
+++ src/jana60/shop/Main.java
@@ -4,16 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Prodotto Cell = new Prodotto("Apple","Iphone", 899, 22 );
+		Prodotto Cell = new Prodotto("Apple","Iphone", 899.34, 22 );
 		Prodotto Computer = new Prodotto("Microsoft", "PersonalPC", 695, 22 );
-		Prodotto VideoCamera = new Prodotto("Canon","I550", 237, 22);
+		Prodotto VideoCamera = new Prodotto("Canon","I550", 237.66, 22);
+		Prodotto PennaUSB = new Prodotto("Lenovo", "7Max", 10.16, 22);
 		
 		
 		
 		
 		System.out.println("Il prezzo del " + Cell.nome + " compreso di Iva è " + Cell.prezzoIvato() + "€");
-		System.out.println("Il prezzo del " + Computer.nome + " di Iva è " + Computer.prezzoIvato() + "€");
-		System.out.println("Il prezzo della " + VideoCamera.nome + " di Iva è " + VideoCamera.prezzoIvato() + "€");
+		System.out.println("Il prezzo del " + Computer.nome + " compreso di Iva è " + Computer.prezzoIvato() + "€");
+		System.out.println("Il prezzo della " + VideoCamera.nome + " compreso di Iva è " + VideoCamera.prezzoIvato() + "€");
+		System.out.println("Il prezzo della " + PennaUSB.nome + " compreso di Iva è " + PennaUSB.prezzoIvato() + "€");
+		System.out.println(Cell.prezzoStringato());
+		System.out.println(Computer.prezzoStringato());
+		System.out.println(VideoCamera.prezzoStringato());
+		System.out.println(PennaUSB.prezzoStringato());
 	}
 
 }
--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package jana60.shop;
 
+import java.text.DecimalFormat;
+
 public class Prodotto {
 
 	//attributi
@@ -25,5 +27,10 @@ public class Prodotto {
 	}
 	
 	//formattare il prezzo restituendo una stringa con un determinato numero di decimali e il carattere €
-	 
+	DecimalFormat prezzoStringato = new DecimalFormat ("##.00");
+		String prezzoStringato () {
+			return ("Il prezzo stringato è " + prezzoStringato.format(prezzoIvato()) + "€");
+		}
+		
+	
 }
