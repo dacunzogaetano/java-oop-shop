e6617256692ad46968b119c80860968b52b72b99
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-15 11:27:54 +0200

    main formattato2

--- src/jana60/shop/Main.java
+++ src/jana60/shop/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Prodotto Cell = new Prodotto("Apple","Iphone", 899, 20 );
+		Prodotto Cell = new Prodotto("Apple","Iphone", 899, 22 );
+		Prodotto Computer = new Prodotto("Microsoft", "PersonalPC", 695, 22 );
+		Prodotto VideoCamera = new Prodotto("Canon","I550", 237, 22);
 		
 		
 		
-		System.out.println("Il prezzo compreso di Iva è " + Cell.prezzoIvato());
+		
+		System.out.println("Il prezzo del " + Cell.nome + " compreso di Iva è " + Cell.prezzoIvato() + "€");
+		System.out.println("Il prezzo del " + Computer.nome + " di Iva è " + Computer.prezzoIvato() + "€");
+		System.out.println("Il prezzo della " + VideoCamera.nome + " di Iva è " + VideoCamera.prezzoIvato() + "€");
 	}
 
 }
